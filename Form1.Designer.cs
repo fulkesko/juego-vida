9ce0a3a541bafb28bf5f6beb7bdf5683567275ae
Flavio Toro B <38701988+fulkesko@users.noreply.github.com>
2019-03-22 22:42:29 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace juego_vida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.picMatriz = new System.Windows.Forms.PictureBox();
+            this.tiempo = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumeroCiclos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picMatriz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(12, 19);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(106, 25);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(401, 23);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(108, 25);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(401, 54);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(108, 25);
+            this.btnDetener.TabIndex = 2;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // picMatriz
+            // 
+            this.picMatriz.Location = new System.Drawing.Point(10, 93);
+            this.picMatriz.Name = "picMatriz";
+            this.picMatriz.Size = new System.Drawing.Size(528, 528);
+            this.picMatriz.TabIndex = 3;
+            this.picMatriz.TabStop = false;
+            this.picMatriz.Click += new System.EventHandler(this.tiempo_Tick);
+            this.picMatriz.Paint += new System.Windows.Forms.PaintEventHandler(this.picMatriz_Paint);
+            // 
+            // tiempo
+            // 
+            this.tiempo.Tick += new System.EventHandler(this.tiempo_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ciclos de vida:";
+            // 
+            // lblNumeroCiclos
+            // 
+            this.lblNumeroCiclos.AutoSize = true;
+            this.lblNumeroCiclos.Location = new System.Drawing.Point(106, 60);
+            this.lblNumeroCiclos.Name = "lblNumeroCiclos";
+            this.lblNumeroCiclos.Size = new System.Drawing.Size(0, 13);
+            this.lblNumeroCiclos.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 633);
+            this.Controls.Add(this.lblNumeroCiclos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picMatriz);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "Form1";
+            this.Text = "Juego de la vida";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picMatriz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.PictureBox picMatriz;
+        private System.Windows.Forms.Timer tiempo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNumeroCiclos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace juego_vida
+{
+    public partial class Form1 : Form
+    {
+        private int longitud = 25;
+        private int largoPixel = 20;
+        int[,] celulas;
+        private int vueltasVida = 0;
+        List<string> lista = new List<string>();
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            celulas = new int[longitud, longitud];
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void PintarMatriz()
+        {   
+            Bitmap bmp = new Bitmap(picMatriz.Width, picMatriz.Height);
+            for (int x = 0; x < longitud; x++)
+            {
+                for (int y = 0; y < longitud; y++)
+                {
+                    if (celulas[x, y] == 0)
+                        pintarPixel(bmp, x, y, Color.White);
+                    else
+                        pintarPixel(bmp, x, y, Color.Black);
+                }
+
+            }
+            
+            picMatriz.Image = bmp;
+        }
+
+        private void pintarPixel(Bitmap bmp, int x, int y, Color color)
+        {
+            for (int i = 0; i < largoPixel; i++)
+                for (int j = 0; j < largoPixel; j++)
+                    bmp.SetPixel(i + ((x * largoPixel)), j + ((y * largoPixel)), color);
+
+        }
+
+        private void JuegoDeLaVida()
+        {
+            int[,] celulasTemporales = new int[longitud, longitud];
+            for (int x = 0; x < longitud; x++)
+            {
+                for (int y = 0; y < longitud; y++)
+                {
+                    if (celulas[x, y] == 0)
+                        celulasTemporales[x, y] = Reglas(x, y, false);
+                    else
+                        celulasTemporales[x, y] = Reglas(x, y, true);
+                }
+            }
+            celulas = celulasTemporales;
+        }
+        private int Reglas(int x, int y, bool viva)
+        {
+            int VecinasVivas = 0;
+
+            //vecina 1
+            if (x > 0 && y > 0)
+                if (celulas[x - 1, y - 1] == 1)
+                    VecinasVivas++;
+
+            //vecina 2
+            if (y > 0)
+                if (celulas[x, y - 1] == 1)
+                    VecinasVivas++;
+
+            //vecina 3
+            if (x < longitud - 1 && y > 0)
+                if (celulas[x + 1, y - 1] == 1)
+                    VecinasVivas++;
+
+            //vecina 4
+            if (x > 0)
+                if (celulas[x - 1, y] == 1)
+                    VecinasVivas++;
+
+            //vecina 5
+            if (x < longitud - 1)
+                if (celulas[x + 1, y] == 1)
+                    VecinasVivas++;
+
+            //vecina 6
+            if (x > 0 && y < longitud - 1)
+                if (celulas[x - 1, y + 1] == 1)
+                    VecinasVivas++;
+
+            //vecina 7
+            if (y < longitud - 1)
+                if (celulas[x, y + 1] == 1)
+                    VecinasVivas++;
+
+
+            //vecina 8
+            if (x < longitud - 1 && y < longitud - 1)
+                if (celulas[x + 1, y + 1] == 1)
+                    VecinasVivas++;
+
+            if (viva)
+                return (VecinasVivas == 2 || VecinasVivas == 3) ? 1 : 0;
+            else
+                return VecinasVivas == 3 ? 1 : 0;
+        }
+
+        private void tiempo_Tick(object sender, EventArgs e)
+        {
+            JuegoDeLaVida();
+            PintarMatriz();
+            vueltasVida++;
+            lblNumeroCiclos.Text = "" + vueltasVida;
+
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            LeerArchivo();
+            
+
+
+
+
+            
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            PintarMatriz();
+            tiempo.Enabled = true;
+            if (btnIniciar.Text == "continuar")
+            { btnIniciar.Text = "Iniciar";
+            }
+            
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            tiempo.Enabled = false;
+            btnIniciar.Text = "continuar";
+        }
+
+        private void CargarMatriz(int valor)
+        {
+            for (int f = 0; f < celulas.Length; f++)
+            {
+                for (int c = 0; c < celulas.Length; c++)
+                {
+
+                    celulas[f, c] = valor;
+                }
+            }
+        }
+        //boton de prueba
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            for (int f = 0; f < longitud; f++)
+            {
+                for (int c = 0; c < longitud; c++)
+                {
+                   Console.Write(celulas[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+            ////reinicio
+            //for (int i = 0; i < longitud; i++)
+            //    for (int j = 0; j < longitud; j++)
+            //       celulas[i, j] = 0;
+            //vueltasVida = 0;
+
+            ////llenado
+            //Random r = new Random();
+
+            //for (int i = 0; i < longitud; i++)
+            //    for (int j = 0; j < longitud; j++)
+            //        celulas[i, j] = r.Next(0, 2);
+            //PintarMatriz();
+            
+
+        }
+        public void LeerArchivo()
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        String val = "";
+                        int f = 0;
+                        int c = 0;
+
+                        foreach (char item in fileContent)
+                        {
+                            val = Convert.ToString(item);
+                            if (val == "1" || val == "0")
+                            {
+                                if (c == 25) {
+                                    break;
+                                }
+                                if(f < 25)
+                                {
+                                    celulas[f, c] = Convert.ToInt32(val);
+                                    f++;
+                                    if (f == 25)
+                                    {
+                                        f = 0;
+                                        c++;
+
+                                    }
+                                }
+                                
+                                
+                                
+                                
+                                
+                            }
+                        }
+
+
+                    }
+
+                }
+            }
+            PintarMatriz();
+        }
+
+        private void picMatriz_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black);
+             
+            for (int y = 0; y<= longitud; ++y)
+            {
+                g.DrawLine(p, 0, y * largoPixel, longitud * largoPixel, y * largoPixel);
+
+            }
+            for (int x = 0; x <= longitud; x++)
+            {
+                g.DrawLine(p, x * largoPixel, 0, x * largoPixel, longitud * largoPixel);
+            }
+            ////hori
+            //for (int i = 500; i >= 0; i = i - longitud)
+            //{
+            //    g.DrawLine(p, 0, i, 500, i);
+
+            //}
+            ////verti
+            //for (int j = 500; j >= 0 ; j= j-longitud)
+            //{
+            //    g.DrawLine(p, j, 0, j, 500);
+            //}
+        }
+
+        //private void btnReset_Click(object sender, EventArgs e)
+        //{
+        //    lblNumeroCiclos.Text = "0";
+        //}
+    }
+    
+}
